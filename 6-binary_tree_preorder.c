f674166b9ec4afdf36f4ea6b8d234f4ef34df1b0
bashthat <3249@holbertonstudents.com>
2022-11-03 22:11:04 -0500

    testfiles

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,6 @@
  * binary_tree_preorder - goes through binary tree using pre-order traversal
  * @tree: pointer to root node of tree to traverse
  * @func: pointer to function to call for each node
- * @int: value in node
  * Return: void
  */
 
