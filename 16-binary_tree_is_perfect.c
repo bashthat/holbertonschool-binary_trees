acc103a4f58f0fa54ace49bf530b42cc069196cf
bashthat <3249@holbertonstudents.com>
2022-11-03 22:29:30 -0500

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,13 +10,29 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+int left_height, right_height;
+if (tree == NULL)
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+return (left_height - right_height);
+}
 
-    if (tree == NULL)
-        return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    return (left_height - right_height);
+/**
+ * binary_tree_is_full - checks if binary tree is full
+ * @tree: pointer to the root node
+ * Return: 1-full, 0therwise 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+if (tree->left != NULL && tree->right != NULL)
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+return (0);
 }
 
 /**
@@ -27,31 +43,32 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
-
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
-        return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+size_t left_height, right_height;
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+if (left_height > right_height)
+return (left_height + 1);
+else
+return (right_height + 1);
 }
 
 /**
  * binary_tree_is_perfect - checks if binary tree is perfect
  * @tree: pointer to the root node
- * Return: 1 if perfect, 0 otherwise
+ * Return: 1 if perfect || 0 if NULL
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    if (binary_tree_is_full(tree) && binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-        return (1);
-    return (0);
+if (tree == NULL)
+return (0);
+if (binary_tree_is_full(tree) == 0)
+return (0);
+if (binary_tree_balance(tree) != 0)
+return (0);
+return (1);
 }
