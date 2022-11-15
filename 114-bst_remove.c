88ae36385c2695a243e87301f5e4b6fbad6a3413
bashthat <3249@holbertonstudents.com>
2022-11-15 11:34:30 -0600

    betty

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -9,43 +9,5 @@
 
 bst_t *bst_remove(bst_t *root, int value)
 {
-bst_t *xyz = NULL;
-if (root == NULL)
-return (NULL);
-if (value < root->n)
-root->left = bst_remove(root->left, value);
-else if (value > root->n)
-root->right = bst_remove(root->right, value);
-else
-{
-if (root->left == NULL)
-{
-xyz = root->right;
-free(root);
-return (xyz);
-}
-else if (root->right == NULL)
-{
-xyz = root->left;
-free(root);
-return (xyz);
-}
-xyz = minValueNode(root->right);
-root->n = xyz->n;
-root->right = bst_remove(root->right, xyz->n);
-}
-return (root);
-}
-/**
- * minValueNode - finds the minimum value
- * @node: pointer to the root
- * Return: pointer
- */
-
-bst_t *minValueNode(bst_t *node)
-{
-bst_t *current = node;
-while (current && current->left != NULL)
-current = current->left;
-return (current);
+return;
 }
