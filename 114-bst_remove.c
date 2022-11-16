fab4ee283a896b179083952debed300c7b9bd437
bashthat <3249@holbertonstudents.com>
2022-11-16 12:40:27 -0600

    editing

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -9,5 +9,27 @@
 
 bst_t *bst_remove(bst_t *root, int value)
 {
-return;
+bst_t *node = NULL, *temp = NULL;
+
+if (root == NULL)
+return (NULL);
+
+node = bst_search(root, value);
+
+if (node == NULL)
+return (NULL);
+
+if (node->left == NULL && node->right == NULL)
+{
+if (node->parent->left == node)
+node->parent->left = NULL;
+else
+node->parent->right = NULL;
+free(node);
+
+return (root);
+}
+
+
+
 }
