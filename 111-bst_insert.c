b3f8573494b7e9d6667d8e6f63ace4896a046332
bashthat <3249@holbertonstudents.com>
2022-11-16 12:58:32 -0600

    editing

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,18 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert - insert a node in a binary search tree
- * @tree:  is a double pointer to the root node and the BST insert value
- * @value: is the value to store and inserted into the tree node
+ * bst_insert - insert a node /// binary search tree
+ * @tree:  is a double pointer to the root node
+ * @value: is the value to store and insert
  * Return: pointer of node, or NULL on failure
  */
 
 bst_t *bst_insert(bst_t **tree, int value)
 
 {
+bst_t *new_node = NULL;
     
-    return (NULL);
+if (tree == NULL)
+return (NULL);
 
+if (*tree == NULL)
+return (*tree = binary_tree_node(NULL, value));
 
+if (value < (*tree)->n)
+return (bst_insert(&(*tree)->left, value));
 
+if (value > (*tree)->n)
+
+return (bst_insert(&(*tree)->right, value));    
+return (NULL);
+    
 }
