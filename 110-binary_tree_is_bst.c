58fcd2a18fd521aa7d651eee6055a911e740d1d9
bashthat <3249@holbertonstudents.com>
2022-11-16 12:51:55 -0600

    errors

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -8,7 +8,30 @@
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+if (tree->left != NULL && tree->right != NULL)
+{
+if (tree->left->n < tree->n && tree->right->n > tree->n)
+return (1);
+}
+if (tree->left != NULL && tree->right == NULL)
+{
+if (tree->left->n < tree->n)
+return (1);
+}
+if (tree->left == NULL && tree->right != NULL)
+{
+if (tree->right->n > tree->n)
+return (1);
+}
+return (0);
+}
 
+
+/*first set*/
 if (tree == NULL)
 return (0);
 if (tree->left != NULL && tree->left->n >= tree->n)
@@ -18,6 +41,7 @@ return (0);
 if (binary_tree_is_bst(tree->left) == 0)
 return (0);
 if (binary_tree_is_bst(tree->right) == 0)
+
 return (0);
 return (1);
 
